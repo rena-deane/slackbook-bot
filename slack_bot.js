063f4d5d9425ead151a4555dfac39a4272aa6399
Rena Goldsmith <rena.kg@gmail.com>
2016-09-18 22:11:06 +1200

    fetch translation from database

--- slack_bot.js
+++ slack_bot.js
@@ -8,21 +8,21 @@ ______     ______     ______   __  __     __     ______
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 if (!process.env.token) {
-  console.log('Error: Specify token in environment');
-  process.exit(1);
+  console.log('Error: Specify token in environment')
+  process.exit(1)
 }
 
-var Botkit = require('./lib/Botkit.js');
-var os = require('os');
+var Botkit = require('./lib/Botkit.js')
+var os = require('os')
 var request = require('superagent')
 
 var controller = Botkit.slackbot({
   debug: true
-});
+})
 
 var bot = controller.spawn({
   token: process.env.token
-}).startRTM();
+}).startRTM()
 
 
 controller.hears(['hello', 'hi', 'Kia ora'], 'direct_message,direct_mention,mention', function(bot, message) {
@@ -33,87 +33,109 @@ controller.hears(['hello', 'hi', 'Kia ora'], 'direct_message,direct_mention,ment
     name: 'robot_face',
   }, function(err, res) {
     if (err) {
-      bot.botkit.log('Failed to add emoji reaction :(', err);
+      bot.botkit.log('Failed to add emoji reaction :(', err)
     }
-  });
+  })
 
 
   controller.storage.users.get(message.user, function(err, user) {
     if (user && user.name) {
-      bot.reply(message, 'Kia ora ' + user.name + '!!');
+      bot.reply(message, 'Kia ora ' + user.name + '!!')
     } else {
-      bot.reply(message, 'Kia ora ko Keretao toku ingoa :robot_face:.  Would you like to translate?');
+      bot.reply(message, 'Kia ora ko Keretao toku ingoa :robot_face:.  Would you like to translate?')
     }
-  });
-});
+  })
+})
 
 
-
-// Hit API
+// MAori API
 controller.hears(['whakamaori'], 'direct_message,direct_mention,mention', function(bot, message) {
 
   controller.storage.users.get(message.user, function(err, user) {
-    bot.startConversation(message, function(err, convo) {
-      if (!err) {
-        convo.ask('What would you like me to translate?', function(response, convo) {
-          convo.ask('Would you like me to translate `' + response.text + '`?', [
-            {
-              pattern: 'ae',
-              callback: function(response, convo) {
-                convo.say('Maku e kimi i te Reo')
-                convo.next();
+    if (!err){
+      bot.startConversation(message, function(err, convo) {
+        if (!err) {
+          convo.ask('Give me a word to translate!', function(response, convo) {
+            convo.ask('Would you like me to whakamaori `' + response.text + '`?', [
+              {
+                pattern: 'ae',
+                callback: function(response, convo) {
+                  convo.say('Tatari mo te kupu/rerenga korero Maori')
+                  convo.next();
+                }
+              },
+              {
+                pattern: 'kao',
+                callback: function(response, convo) {
+                  convo.say('Maybe another time then')
+                  convo.stop();
+                }
+              },
+              {
+                default: true,
+                callback: function(response, convo) {
+                  convo.repeat();
+                  convo.next();
+                }
               }
-            },
-            {
-              pattern: 'kao',
-              callback: function(response, convo) {
-                convo.say('Random quote - create a new quote table')
-                convo.stop();
-              }
-            },
-            {
-              default: true,
-              callback: function(response, convo) {
-                convo.repeat();
-                convo.next();
-              }
-            }
-          ]);
+            ]);
 
-          convo.next();
+            convo.next();
 
-        }, {'key': 'word'}); // store the results in a field called nickname
+          }, {'key': 'word'}); // store the results in a field called word
 
-        convo.on('end', function(convo) {
-          if (convo.status == 'completed') {
-            controller.storage.users.get(message.user, function(err, user) {
-              if (!user) {
-                user = {
-                  id: message.user,
-                };
-              }
-              user.name = convo.extractResponse('word');
-              controller.storage.users.save(user, function(err, id) {
-                request.get('https://test-papakupu.herokuapp.com/v1/translations/1.1')
-                  .then((data) => {
-                    var english = JSON.parse(data.body[0])
-                    bot.reply(message, 'word is: ' + english);
-                    console.log('api stuff here: ', english);
-                  })
-                  .catch((err) => {
-                    bot.reply(message, 'Sorry I could not find a transalation for ' + word);
-                  })
+          convo.on('end', function(convo) {
+            if (convo.status == 'completed') {
+              bot.reply(message, '...................');
+              controller.storage.users.get(message.user, function(err, user) {
+                if (!user) {
+                  user = {
+                    id: message.user,
+                  };
+                }
+                user.word = convo.extractResponse('word');
+
+                bot.startConversation(message, function(err, convo) {
+                  request.get('https://test-papakupu.herokuapp.com/v1/translations/1.1')
+                    .then(function(dictionary){
+                      var words = dictionary.body
+                      words.map(function(word){
+                        console.log(word)
+                        if(word.english === user.word){
+                          convo.say('`' + user.word + '`: ' + '`' + word.maori + '`')
+                          convo.say('...................')
+                          convo.say('```' + word.maori_sentence + '```')
+                          convo.say('```' + word.english_sentence + '```')
+
+                        }
+                      })
+                      // for(var entry in dictionary.body){
+                      //   console.log('entry', entry)
+                      //   console.log('typeofentry', typeof entry)
+                      //   if (user.word === entry.english){
+                      //     convo.say('Anei te kupu Māori mo ' + user.word + ':' + '*' + entry.maori + '*')
+                      //   }
+                      // }
+                    })
+                    .catch(function(err){
+                      convo.say('kaore taea te whakamaori :expressionless:')
+                    })
+                })
+
+
               });
-            });
 
-          } else {
-            // this happens if the conversation ended prematurely for some reason
-            bot.reply(message, 'Another time then :robot_face:');
-          }
-        });
-      }
-    });
-  })
+
+
+            } else {
+              // this happens if the conversation ended prematurely for some reason
+              bot.reply(message, 'Kei te pai!');
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
 
@@ -125,52 +147,52 @@ controller.hears(['shutdown', 'goodnight'], 'direct_message,direct_mention,menti
       {
         pattern: bot.utterances.yes,
         callback: function(response, convo) {
-          convo.say('Bye!');
-          convo.next();
+          convo.say('Bye!')
+          convo.next()
           setTimeout(function() {
-            process.exit();
-          }, 3000);
+            process.exit()
+          }, 3000)
         }
       },
       {
         pattern: bot.utterances.no,
         default: true,
         callback: function(response, convo) {
-          convo.say('*Phew!*');
-          convo.next();
+          convo.say('*Phew!*')
+          convo.next()
         }
       }
-    ]);
-  });
-});
+    ])
+  })
+})
 
 
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name', 'ko wai koe'],
 'direct_message,direct_mention,mention', function(bot, message) {
 
-  var hostname = os.hostname();
-  var uptime = formatUptime(process.uptime());
+  var hostname = os.hostname()
+  var uptime = formatUptime(process.uptime())
 
   bot.reply(message,
     ':robot_face: I am a bot named <@' + bot.identity.name +
-    '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+    '>. I have been running for ' + uptime + ' on ' + hostname + '.')
 
-  });
+  })
 
   function formatUptime(uptime) {
-    var unit = 'second';
+    var unit = 'second'
     if (uptime > 60) {
-      uptime = uptime / 60;
-      unit = 'minute';
+      uptime = uptime / 60
+      unit = 'minute'
     }
     if (uptime > 60) {
-      uptime = uptime / 60;
-      unit = 'hour';
+      uptime = uptime / 60
+      unit = 'hour'
     }
     if (uptime != 1) {
-      unit = unit + 's';
+      unit = unit + 's'
     }
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
+    uptime = uptime + ' ' + unit
+    return uptime
   }
